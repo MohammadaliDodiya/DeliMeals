89174062e32b2e3ad0bf743e209c252bc68530fa
KurbanAli Masu <72492660+KurbanAli6615@users.noreply.github.com>
2021-10-26 22:03:17 +0530

    Update dummy_data.dart

--- deli_meals/lib/widgets/dummy_data.dart
+++ deli_meals/lib/widgets/dummy_data.dart
@@ -18,11 +18,7 @@ const DUMMY_CATEGORIES = [
     title: 'Hamburgers',
     color: Colors.orange,
   ),
-  Category(
-    id: 'c4',
-    title: 'German',
-    color: Colors.amber,
-  ),
+
   Category(
     id: 'c5',
     title: 'Light & Lovely',
@@ -43,11 +39,7 @@ const DUMMY_CATEGORIES = [
     title: 'Asian',
     color: Colors.lightGreen,
   ),
-  Category(
-    id: 'c9',
-    title: 'French',
-    color: Colors.pink,
-  ),
+
   Category(
     id: 'c10',
     title: 'Summer',
@@ -369,39 +361,5 @@ const DUMMY_MEALS = [
     isVegan: false,
     isVegetarian: true,
     isLactoseFree: false,
-  ),
-  Meal(
-    id: 'm10',
-    categories: [
-      'c2',
-      'c5',
-      'c10',
-    ],
-    title: 'Asparagus Salad with Cherry Tomatoes',
-    affordability: Affordability.Luxurious,
-    complexity: Complexity.Simple,
-    imageUrl:
-        'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
-    duration: 30,
-    ingredients: [
-      'White and Green Asparagus',
-      '30g Pine Nuts',
-      '300g Cherry Tomatoes',
-      'Salad',
-      'Salt, Pepper and Olive Oil'
-    ],
-    steps: [
-      'Wash, peel and cut the asparagus',
-      'Cook in salted water',
-      'Salt and pepper the asparagus',
-      'Roast the pine nuts',
-      'Halve the tomatoes',
-      'Mix with asparagus, salad and dressing',
-      'Serve with Baguette'
-    ],
-    isGlutenFree: true,
-    isVegan: true,
-    isVegetarian: true,
-    isLactoseFree: true,
   ),
 ];
